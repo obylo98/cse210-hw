84291114a296a5bcb94cf034e32b4ab164fb1dc7
obylo98 <obinnaandrewonyekwere@gmail.com>
2024-07-20 05:33:27 +0000

    foundation 1-4

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,92 @@
 using System;
+using System.Collections.Generic;
+
+class Video
+{
+    private string _title;
+    private string _author;
+    private int _length; // in seconds
+    private List<Comment> _comments;
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_length} seconds");
+        Console.WriteLine($"Number of comments: {GetNumberOfComments()}");
+
+        Console.WriteLine("Comments:");
+        foreach (var comment in _comments)
+        {
+            comment.DisplayComment();
+        }
+        Console.WriteLine();
+    }
+}
+
+class Comment
+{
+    private string _name;
+    private string _text;
+
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+
+    public void DisplayComment()
+    {
+        Console.WriteLine($"{_name}: {_text}");
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Video 1", "Author 1", 300);
+        video1.AddComment(new Comment("User1", "Great video!"));
+        video1.AddComment(new Comment("User2", "Very informative."));
+        video1.AddComment(new Comment("User3", "Thanks for sharing."));
+
+        Video video2 = new Video("Video 2", "Author 2", 600);
+        video2.AddComment(new Comment("User4", "Nice content."));
+        video2.AddComment(new Comment("User5", "Helpful tips!"));
+        video2.AddComment(new Comment("User6", "Awesome!"));
+
+        Video video3 = new Video("Video 3", "Author 3", 450);
+        video3.AddComment(new Comment("User7", "Well done!"));
+        video3.AddComment(new Comment("User8", "Very useful."));
+        video3.AddComment(new Comment("User9", "Loved it!"));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,145 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    protected string _date;
+    protected int _lengthInMinutes;
+
+    public Activity(string date, int lengthInMinutes)
+    {
+        _date = date;
+        _lengthInMinutes = lengthInMinutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} Activity ({_lengthInMinutes} min)";
+    }
+}
+
+class Running : Activity
+{
+    private double _distance;
+
+    public Running(string date, int lengthInMinutes, double distance)
+        : base(date, lengthInMinutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (_distance / _lengthInMinutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return _lengthInMinutes / _distance;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {_distance} miles, Speed: {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min/mile";
+    }
+}
+
+class Cycling : Activity
+{
+    private double _speed;
+
+    public Cycling(string date, int lengthInMinutes, double speed)
+        : base(date, lengthInMinutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (_speed * _lengthInMinutes) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {GetDistance():0.0} miles, Speed: {_speed:0.0} mph, Pace: {GetPace():0.0} min/mile";
+    }
+}
+
+class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int lengthInMinutes, int laps)
+        : base(date, lengthInMinutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return (_laps * 50) / 1000.0 * 0.62; 
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / _lengthInMinutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return _lengthInMinutes / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {GetDistance():0.0} miles, Speed: {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min/mile";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("2024-07-20", 30, 3.0),
+            new Cycling("2024-07-21", 45, 15.0),
+            new Swimming("2024-07-22", 60, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
